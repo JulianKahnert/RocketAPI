76038e0661cb05d671aa69cdf19735f18f3961da
Julian Kahnert <mail@juliankahnert.de>
2016-08-25 11:06:45 +0200

    GUI: better UI

--- RocketGUI.py
+++ RocketGUI.py
@@ -3,6 +3,16 @@
 from bottle import route, run, template, request
 
 from rocket_state import machine_state
+from rocket_state import Language
+from rocket_state import DayOfWeek
+from rocket_state import TemperatureUnit
+from rocket_state import WaterSource
+from rocket_state import ActiveProfile
+
+from rocket_state import Coffee_temp_C
+from rocket_state import Coffee_temp_F
+from rocket_state import Steam_temp_C
+from rocket_state import Steam_temp_F
 
 
 @route('/')
@@ -22,19 +32,81 @@ def test():
     <body>
     <h1>Rocket R60V - Machine State</h1>
        <p>This website serves a tiny interface for your RocketR60V.</p>
+    <form>
     <table style="width:80%">
       <tr>
         <th>Setting</th><th>current state</th><th>new state</th>
       </tr>
     """
+    # coffeeCyclesSubtotal
+    html += '<tr><td>coffeeCyclesSubtotal</td><td>{0}</td><td>{0}</td></tr>'.format(state.coffeeCyclesSubtotal)
+    # coffeeCyclesTotal
+    html += '<tr><td>coffeeCyclesTotal</td><td>{0}</td><td>{0}</td></tr>'.format(state.coffeeCyclesTotal)
+    # pressureA
+    html += '<tr><td>pressureA</td><td>{0}</td><td contenteditable="true">{0}</td></tr>'.format(state.pressureA)
+    # pressureB
+    html += '<tr><td>pressureB</td><td>{0}</td><td contenteditable="true">{0}</td></tr>'.format(state.pressureB)
+    # pressureC
+    html += '<tr><td>pressureC</td><td>{0}</td><td contenteditable="true">{0}</td></tr>'.format(state.pressureC)
+    # activeProfile
+    html += '<tr><td>activeProfile</td><td>{}</td><td><select name="activeProfile">'.format(state.activeProfile) + \
+        ''.join(['<option>{}</option>'.format(sz) for sz in ActiveProfile]) + '</select></td></tr>'
+    # language
+    html += '<tr><td>language</td><td>{}</td><td><select name="language">'.format(state.language) + \
+        ''.join(['<option>{}</option>'.format(sz) for sz in Language]) + '</select></td></tr>'
+    # isServiceBoilerOn
+    html += '<tr><td>isServiceBoilerOn</td><td>{}</td><td><input type="checkbox" name="isServiceBoilerOn" checked></td></tr>'.format(state.isServiceBoilerOn)
 
-    for key in my_dict.keys():
-            html += template('<tr><td>{0}</td><td>{{{{{0}}}}}</td><td contenteditable="true">{{{{{0}}}}}</td></tr>'.format(key), **my_dict)
+    # isMachineInStandby
+    html += '<tr><td>isMachineInStandby</td><td>{}</td><td><input type="checkbox" name="isMachineInStandby" unchecked></td></tr>'.format(state.isServiceBoilerOn)
+
+    # waterSource
+    html += '<tr><td>waterSource</td><td>{}</td><td><select name="waterSource">'.format(state.waterSource) + \
+        ''.join(['<option>{}</option>'.format(sz) for sz in WaterSource]) + '</select></td></tr>'
+
+    # temperatureUnit
+    html += '<tr><td>temperatureUnit</td><td>{}</td><td><select name="temperatureUnit">'.format(state.temperatureUnit) + \
+        ''.join(['<option>{}</option>'.format(sz) for sz in TemperatureUnit]) + '</select></td></tr>'
+
+    # coffeeTemperature
+    html += '<tr><td>coffeeTemperature</td><td>{}</td><td>'.format(state.coffeeTemperature)
+    if state.temperatureUnit == 'Celsius':
+        html += '<input type="number" value="{}" min="{t[0]}" max="{t[1]}" step="1" name="coffeeTemperature"/></td></tr>'.format(state.coffeeTemperature, t=Coffee_temp_C)
+    else:
+        html += '<input type="number" value="{}" min="{t[0]}" max="{t[1]}" step="1" name="coffeeTemperature"/></td></tr>'.format(state.coffeeTemperature, t=Coffee_temp_F)
+
+    # steamTemperature
+    html += '<tr><td>steamTemperature</td><td>{}</td><td>'.format(state.steamTemperature)
+    if state.temperatureUnit == 'Celsius':
+        html += '<input type="number" value="{0}" min="{t[0]}" max="{t[1]}" step="1" name="steamTemperature"/></td></tr>'.format(state.steamTemperature, t=Steam_temp_C)
+    else:
+        html += '<input type="number" value="{}" min="{t[0]}" max="{t[1]}" step="1" name="steamTemperature"/></td></tr>'.format(state.steamTemperature, t=Steam_temp_F)
+
+    # steamCleanTime
+    html += '<tr><td>steamCleanTime</td><td>{0}</td><td>{0}</td></tr>'.format(state.steamCleanTime)
+
+    # coffeePID
+    html += '<tr><td>coffeePID</td><td>{0}</td><td>{0}</td></tr>'.format(state.coffeePID)
+
+    # groupPID
+    html += '<tr><td>groupPID</td><td>{0}</td><td>{0}</td></tr>'.format(state.groupPID)
+
+    # mysteryPID
+    html += '<tr><td>mysteryPID</td><td>{0}</td><td>{0}</td></tr>'.format(state.mysteryPID)
+
+    # autoOnTime
+    html += '<tr><td>autoOnTime</td><td>{0}</td><td>{0}</td></tr>'.format(state.autoOnTime)
+
+    # autoStandbyTime
+    html += '<tr><td>autoStandbyTime</td><td>{0}</td><td>{0}</td></tr>'.format(state.autoStandbyTime)
+
+    # autoSkipDay
+    html += '<tr><td>autoSkipDay</td><td>{0}</td><td>{0}</td></tr>'.format(state.autoSkipDay)
 
     html += """
     </table>
-
-    <button id="export-btn" class="btn btn-primary">Update State</button><p id="export"></p>
+    <input value="Update Sate" type="submit" />
+    </form>
 
     </body>
     </html>
