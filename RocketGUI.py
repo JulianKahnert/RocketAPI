972d6476472176d69cb0e8798e19bbffc1e417ab
Julian Kahnert <mail@juliankahnert.de>
2016-08-25 09:54:44 +0200

    GUI: table with properties

--- RocketGUI.py
+++ RocketGUI.py
@@ -1,53 +1,44 @@
 #!/usr/bin/env python3
 
-from bottle import route, run
+from bottle import route, run, template, request
 
+from rocket_state import machine_state
 
-@route('/hello')
-def hello():
-    return "Hello World!"
-
-
-@route('/test')
-def test():
-    html = '<input type="range" min="0" max="100">'
-    return html
 
 @route('/')
 def test():
+    state = machine_state()
+    my_dict = dict((name, getattr(state, name)) for name in dir(state) if not name.startswith('_'))
     html = """
-	<!DOCTYPE html>
-	<html>
-	<head>
-	<style>
-	table, th, td {
-	    border: 1px solid black;
-	}
-	</style>
-	</head>
-	<body>
-	<h1>Rocket R60V - Machine State</h1>
-	   <p>This website serves a tiny interface for your RocketR60V.</p>
-	<table style="width:100%">
-	  <tr>
-	    <th>Setting</th><th>current state</th><th>new state</th>
-	  </tr>
-	  <tr>
-	    <td>Jill</td><td>Smith</td><td contenteditable="true">50</td>
-	  </tr>
-	  <tr>
-	    <td>Eve</td><td>Jackson</td><td contenteditable="true">94</td>
-	  </tr>
-	  <tr>
-	    <td>John</td><td>Doe</td><td contenteditable="true">80</td>
-	  </tr>
-	</table>
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table, th, td {
+        border: 1px solid black;
+    }
+    </style>
+    </head>
+    <body>
+    <h1>Rocket R60V - Machine State</h1>
+       <p>This website serves a tiny interface for your RocketR60V.</p>
+    <table style="width:80%">
+      <tr>
+        <th>Setting</th><th>current state</th><th>new state</th>
+      </tr>
+    """
 
-	<button id="export-btn" class="btn btn-primary">Update State</button><p id="export"></p>
+    for key in my_dict.keys():
+            html += template('<tr><td>{0}</td><td>{{{{{0}}}}}</td><td contenteditable="true">{{{{{0}}}}}</td></tr>'.format(key), **my_dict)
 
-	</body>
-	</html>
-	"""
+    html += """
+    </table>
+
+    <button id="export-btn" class="btn btn-primary">Update State</button><p id="export"></p>
+
+    </body>
+    </html>
+    """
     return html
 
 
