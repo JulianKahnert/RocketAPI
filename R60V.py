7c1acc0a0f2c74a33e54e6ec3cb4741e633e9a30
Julian Kahnert <mail@juliankahnert.de>
2016-10-01 15:19:14 +0200

    first cli approach + pressure profiles

--- R60V.py
+++ R60V.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
+import logging
+import numpy as np
 import socket
 import time
 
@@ -19,7 +22,11 @@ class api:
         self.s = socket.create_connection((self.machine_ip, self.machine_port), 10)
 
         # get first "hello" from machine
-        print(self.s.recv(self.buffer_size))
+        if self.s.recv(self.buffer_size) == b'*HELLO*':
+            logging.info('Connection established!')
+        else:
+            logging.critical('ERROR: machine not reachable')
+            return
 
         # waiting time seems to be important here
         time.sleep(1)
@@ -29,6 +36,7 @@ class api:
         Destructor
         """
         self.s.close()
+        logging.info('Connection closed!')
 
     def read(self, idx):
         """
@@ -39,12 +47,15 @@ class api:
         request = bytes(request, 'utf-8')
         
         # send request
-        #print('-> {}'.format(request))
+        logging.info('-> {}'.format(request))
         self.s.send(request)
 
         # recieve data
-        raw = self.s.recv(self.buffer_size)
-        #print('<- {}'.format(raw))
+        try:
+            raw = self.s.recv(self.buffer_size)
+        except timeout:
+            logging.error('connection timed out')
+        logging.info('<- {}'.format(raw))
 
         # get date from message
         if self._cs_verify(raw):
@@ -52,11 +63,11 @@ class api:
             data = raw.split(request[:-2])[1][:-2]
             value = int(data, 16)
         else:
-            print('ERROR IN CHECKSUM!')
+            logging.error('ERROR IN CHECKSUM!')
             value = []
 
         # not very nice, but more reliable:
-        time.sleep(0.1)
+        time.sleep(0.2)
 
         return value
 
@@ -64,11 +75,6 @@ class api:
         """
         write data on machine...
         """
-        ## off
-        ## request = self._cs_attach('w' + format(idx, '04X') + format(1, '04X') + '01')
-        ## on
-        ## request = self._cs_attach('w' + format(idx, '04X') + format(1, '04X') + '00')
-        
         # create request
         request = self._cs_attach('w' + format(idx, '04X') + format(1, '04X') + format(value, '02X'))
         request = bytes(request, 'utf-8')
@@ -108,16 +114,12 @@ class api:
 
 
 class state:
-    # create connection to machine
-    obj = api(machine_ip='192.168.1.1', machine_port=1774)
-
-
     # BYTE 0: temperature unit
     TemperatureUnit = ['Celsius', 'Fahrenheit']
     
     def _set_temperatureUnit(self, x):
         if x not in self.TemperatureUnit:
-            print('"{}" is not valid. Choose one of: {}!'.format(x, self.TemperatureUnit))
+            logging.warning('"{}" is not valid. Choose one of: {}!'.format(x, self.TemperatureUnit))
         else:
             self.obj.write(0, self.TemperatureUnit.index(x))
 
@@ -132,7 +134,7 @@ class state:
     
     def _set_language(self, x):
         if x not in self.Language:
-            print('"{}" is not valid. Choose one of: {}!'.format(x, self.Language))
+            logging.warning('"{}" is not valid. Choose one of: {}!'.format(x, self.Language))
         else:
             self.obj.write(1, self.Language.index(x))
 
@@ -158,7 +160,7 @@ class state:
             err = 'Unit has a wrong state "{}"'.format(self.temperatureUnit)
 
         if not bValid:
-            print('Temperature ' + err)
+            logging.warning('Temperature ' + err)
         else:
             self.obj.write(2, x)
 
@@ -184,7 +186,7 @@ class state:
             err = 'unit has a wrong state "{}"'.format(self.temperatureUnit)
 
         if not bValid:
-            print('Temperature ' + err)
+            logging.warning('Temperature ' + err)
         else:
             self.obj.write(3, x)
 
@@ -198,6 +200,18 @@ class state:
     # BYTE 6: xxPID     # 6-7, 12-13, 18-19
     # BYTE 8: xxPID     # 8-9, 14-15, 20-21
     # BYTE 22: pressure profile A       # 22-36
+    def _set_pressureA(self, profile):
+        bValid, err = self._check_profile(profile)
+        if not bValid:
+            logging.warning(err)
+        else:
+            self._write_profile(22, profile)
+
+    pressureA = property(
+        fset=_set_pressureA,
+        fget=lambda self: self._read_profile(22),
+        fdel=None,
+        doc='Pressure profile A - 5 times [seconds, bars]')
     # BYTE 38: pressure profile B       # 38-52
     # BYTE 54: pressure profile C       # 54-68
     
@@ -206,7 +220,7 @@ class state:
     
     def _set_waterSource(self, x):
         if x not in self.WaterSource:
-            print('"{}" is not valid. Choose one of: {}!'.format(x, self.WaterSource))
+            logging.warning('"{}" is not valid. Choose one of: {}!'.format(x, self.WaterSource))
         else:
             self.obj.write(70, self.WaterSource.index(x))
 
@@ -221,7 +235,7 @@ class state:
 
     def _set_activeProfile(self, x):
         if x not in self.ActiveProfile:
-            print('"{}" is not valid. Choose one of: {}!'.format(x, self.ActiveProfile))
+            logging.warning('"{}" is not valid. Choose one of: {}!'.format(x, self.ActiveProfile))
         else:
             self.obj.write(71, self.ActiveProfile.index(x))
     
@@ -235,7 +249,7 @@ class state:
     # BYTE 73: is service boiler on
     def _set_isServiceBoilerOn(self, x):
         if not isinstance(x, bool):
-            print('"{}" is not valid. Choose a bool!'.format(x))
+            logging.warning('"{}" is not valid. Choose a bool!'.format(x))
         else:
             self.obj.write(73, x)
 
@@ -248,7 +262,7 @@ class state:
     # BYTE 74: is machine in standby
     def _set_isMachineInStandby(self, x):
         if not isinstance(x, bool):
-            print('"{}" is not valid. Choose a bool!'.format(x))
+            logging.warning('"{}" is not valid. Choose a bool!'.format(x))
         else:
             self.obj.write(74, x)            
 
@@ -258,14 +272,52 @@ class state:
         fdel=None,
         doc='Standby mode of R60V: on/off')
 
-    # BYTE 75: coffee cycles subtotal   # 75-76
-    # BYTE 77: coffee cycles total      # 77-80
-    # BYTE 81: auto on time     # 81-82
-    # BYTE 83: auto standby time    # 83-84
-    # BYTE 85: auto skip day
+    # BYTE 75: NOT TESTED coffee cycles subtotal   # 75-76
+    coffeeCyclesSubtotal = property(
+        fset=None,
+        fget=lambda self: [self.obj.read(75), self.obj.read(76)],
+        fdel=None,
+        doc='')
+
+    # BYTE 77: NOT TESTED coffee cycles total      # 77-80
+    coffeeCyclesTotal = property(
+        fset=None,
+        fget=lambda self: [self.obj.read(idx) for idx in range(77, 81)],
+        fdel=None,
+        doc='')
+
+    # BYTE 81: NOT TESTED auto on time     # 81-82
+    autoOnTime = property(
+        fset=None,
+        fget=lambda self: [self.obj.read(81), self.obj.read(82)],
+        fdel=None,
+        doc='')
+
+    # BYTE 83: NOT TESTED auto standby time    # 83-84
+    autoStandbyTime = property(
+        fset=None,
+        fget=lambda self: [self.obj.read(83), self.obj.read(84)],
+        fdel=None,
+        doc='')
+
+    # BYTE 85: NOT TESTED auto skip day
+    autoSkipDay = property(
+        fset=None,
+        fget=lambda self: self.obj.read(85),
+        fdel=None,
+        doc='')
 
 
     # ### helper functions ###
+    def __init__(self, machine_ip='192.168.1.1', machine_port=1774):
+        # write a log file
+        logging.basicConfig(filename='myapp.log', level=logging.INFO)
+
+        # create connection to machine
+        self.obj = api(machine_ip=machine_ip, machine_port=machine_port)
+
+    def __del__(self):
+        del self.obj
 
     def _check_range(self, selected, min_max):
         if min_max[0] <= selected <= min_max[1]:
@@ -278,4 +330,91 @@ class state:
                 selected, min_max[0], min_max[1])
             
         return bValid, err
+    
+    def _check_profile(self, profile):
+        # set default values
+        err = []
+        bValid = True
+        # (kind of) proteced MIN/MAX values
+        Pressure = [0, 14]  # bars
+        Time = [0, 60]      # seconds
+        # look at each of the 5 settings
+        for num in [0, 1, 2, 3, 4]:
+            # check temperature
+            bValid, err = self._check_range(profile[num][0], Time)
+            if not bValid:
+                err = 'Time ' + err
+                break
+            # check pressure
+            bValid, err = self._check_range(profile[num][1], Pressure)
+            if not bValid:
+                err = 'Pressure ' + err
+                break
+        return bValid, err
 
+    def _read_profile(self, offset):
+        profile = []
+        profile.append([self.obj.read(offset + 0), self.obj.read(offset + 10)])
+        profile.append([self.obj.read(offset + 2), self.obj.read(offset + 11)])
+        profile.append([self.obj.read(offset + 4), self.obj.read(offset + 12)])
+        profile.append([self.obj.read(offset + 6), self.obj.read(offset + 13)])
+        profile.append([self.obj.read(offset + 8), self.obj.read(offset + 14)])
+        return np.array(profile) / 10
+
+    def _write_profile(self, offset, profile):
+        p = np.array(profile) * 10
+        self.obj.write(offset + 0, p[0][0]), self.obj.write(offset + 10, p[0][1])
+        self.obj.write(offset + 2, p[1][0]), self.obj.write(offset + 11, p[1][1])
+        self.obj.write(offset + 4, p[2][0]), self.obj.write(offset + 12, p[2][1])
+        self.obj.write(offset + 6, p[3][0]), self.obj.write(offset + 13, p[3][1])
+        self.obj.write(offset + 8, p[4][0]), self.obj.write(offset + 14, p[4][1])
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Commandline tool to read and write data from R60V.')
+    
+    parser.add_argument('-on', '--on',
+                        dest='on',
+                        action='store_true',
+                        help='start the machine')
+
+    parser.add_argument('-off', '--off',
+                        dest='off',
+                        action='store_true',
+                        help='shut down the machine')
+
+    parser.add_argument('-r', '--read',
+                        dest='read',
+                        action='store',
+                        help='read machine state')
+
+    parser.add_argument('-s', '--set',
+                        dest='setting',
+                        nargs=2,
+                        action='store',
+                        help='change settings with a key-value pair')
+    args = parser.parse_args()
+    obj = state()
+    if args.on:
+        print('Start the machine ...')
+        obj.isMachineInStandby = False
+
+    if args.off:
+        print('Shutting down the machine ...')
+        obj.isMachineInStandby = True
+
+    if args.read:
+        print(getattr(obj, args.read))
+
+    if args.setting:
+        prop = args.setting[0]
+        val = args.setting[1]
+        if prop in dir(state):
+            if prop in ['coffeeTemperature', 'isMachineInStandby', 'isServiceBoilerOn', 'steamTemperature']:
+                val = int(val)
+            print('Setting attribute "{}"" to "{}".'.format(prop, val))
+            setattr(obj, args.setting[0], args.setting[1])
+        else:
+            print('ERROR: property not in machine state.')
+    
+    del obj
